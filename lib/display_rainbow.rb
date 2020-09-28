cd79aeeeb1ad0c3ba765b6bd48eeafd938f1ea8b
Hannah Malm <hannahemalm616@gmail.com>
2020-09-28 22:13:23 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@
 #end
  
 def display_rainbow(colors)
-  R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
 
 #def display_rainbow(colors)
